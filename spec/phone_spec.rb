af2216b6b99bb4540ccd4ab608bc035c1155ebf5
gracelauren <gracelauren909@gmail.com>
2015-01-19 14:03:09 -0800

    Update phone_spec.rb
    allows for type of phone

--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -5,10 +5,17 @@ describe(Phone) do
   before() do
     Phone.clear()
   end
+  
+    describe("#type") do
+    it("returns the type of the new number of the phone") do
+      test_phone = Phone.new("cell", 555-555-5555")
+      expect(test_phone.new_number()).to(eq("cell"))
+    end
+  end
 
   describe("#new_number") do
     it("returns the new number of the phone") do
-      test_phone = Phone.new("555-555-5555")
+      test_phone = Phone.new("cell", 555-555-5555")
       expect(test_phone.new_number()).to(eq("555-555-5555"))
     end
   end
@@ -22,7 +29,7 @@ describe(Phone) do
 
   describe("#save") do
     it("adds a new_number to the array of saved new_numbers") do
-    test_phone = Phone.new("555-555-5555")
+    test_phone = Phone.new("cell", "555-555-5555")
     test_phone.save()
       expect(Phone.all()).to(eq([test_phone]))
     end
@@ -30,7 +37,7 @@ describe(Phone) do
 
   describe(".clear") do
     it("empty out all of the saved phones") do
-      test_phone = Phone.new("phone").save()
+      test_phone = Phone.new("cell", "555-555-5555").save()
       Phone.clear()
       expect(Phone.all()).to(eq([]))
     end
